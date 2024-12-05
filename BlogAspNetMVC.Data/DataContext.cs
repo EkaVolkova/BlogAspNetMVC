5c0483df560d99de2d053421ef25d18159d283c4
Ekaterina Volkova <koweevaea@gmail.com>
2024-12-05 20:53:25 +0500

    Добавила класс для моделирования базы данных

---
+++ BlogAspNetMVC.Data/DataContext.cs
@@ -0,0 +1,54 @@
+﻿using BlogAspNetMVC.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogAspNetMVC.Data
+{
+    public class DataContext : DbContext
+    {
+        /// <summary>
+        /// Dataset для статей
+        /// </summary>
+        public DbSet<Article> Articles { get; set; }
+
+        /// <summary>
+        /// Dataset для комментариев
+        /// </summary>
+        public DbSet<Comment> Comments { get; set; }
+
+        /// <summary>
+        /// Dataset для комментариев
+        /// </summary>
+        public DbSet<Tag> Tags { get; set; }
+
+        /// <summary>
+        /// Dataset для комментариев
+        /// </summary>
+        public DbSet<User> Users { get; set; }
+
+        /// <summary>
+        /// Создание базы данных, если ее нет
+        /// </summary>
+        /// <param name="options"></param>
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        /// <summary>
+        /// Создание таблиц в базе данных на основе моделей 
+        /// </summary>
+        /// <param name="modelBuilder"></param>
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Article>().ToTable("Articles");
+            modelBuilder.Entity<Comment>().ToTable("Comments");
+            modelBuilder.Entity<Tag>().ToTable("Tags");
+            modelBuilder.Entity<User>().ToTable("Users");
+        }
+    }
+}
