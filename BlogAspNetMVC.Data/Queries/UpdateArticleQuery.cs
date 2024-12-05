de13e979d9d2fe260d96af97820d1d1524b1e28a
Ekaterina Volkova <koweevaea@gmail.com>
2024-12-05 21:44:03 +0500

    Добавила запрос на обновление статьи

---
+++ BlogAspNetMVC.Data/Queries/UpdateArticleQuery.cs
@@ -0,0 +1,34 @@
+﻿using BlogAspNetMVC.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogAspNetMVC.Data.Queries
+{
+    public class UpdateArticleQuery
+    {
+        /// <summary>
+        /// Новое название статьи
+        /// </summary>
+        public string NewName { get; set; }
+
+        /// <summary>
+        /// Новый текст статьи
+        /// </summary>
+        public string NewText { get; set; }
+
+        /// <summary>
+        /// Новый список тегов
+        /// </summary>
+        public List<Tag> NewTags { get; set; }
+
+        public UpdateArticleQuery(string newName = null, string newText = null, List<Tag> newTags = null)
+        {
+            NewName = newName;
+            NewText = newText;
+            NewTags = newTags;
+        }
+    }
+}
