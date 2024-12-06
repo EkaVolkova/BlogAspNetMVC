f34f6816d73338ed974bcfba59a718e2047a9407
Ekaterina Volkova <koweevaea@gmail.com>
2024-12-06 20:20:40 +0500

    Добавила запрос на обновление тега

---
+++ BlogAspNetMVC.Data/Queries/UpdateTagQuery.cs
@@ -0,0 +1,16 @@
+﻿namespace BlogAspNetMVC.Data.Queries
+{
+    public class UpdateTagQuery
+    {
+
+        /// <summary>
+        /// Новое название тега
+        /// </summary>
+        public string NewName { get; set; }
+
+        public UpdateTagQuery(string newName = null)
+        {
+            NewName = newName;
+        }
+    }
+}
