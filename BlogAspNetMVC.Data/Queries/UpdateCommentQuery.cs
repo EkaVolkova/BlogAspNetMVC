69b3ff6b080151d9734842e7f99964afc3798f69
Ekaterina Volkova <koweevaea@gmail.com>
2024-12-06 20:07:53 +0500

    Добавила запрос на обновление комментария

---
+++ BlogAspNetMVC.Data/Queries/UpdateCommentQuery.cs
@@ -0,0 +1,20 @@
+﻿using BlogAspNetMVC.Data.Models;
+using System.Collections.Generic;
+
+namespace BlogAspNetMVC.Data.Queries
+{
+    public class UpdateCommentQuery
+    {
+
+        /// <summary>
+        /// Новый текст комментария
+        /// </summary>
+        public string NewText { get; set; }
+
+        public UpdateCommentQuery (string newText = null)
+        {
+            NewText = newText;
+        }
+    }
+
+}
