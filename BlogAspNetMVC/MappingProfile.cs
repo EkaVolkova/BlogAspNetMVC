43a17f52b5e9987d1be75c1d5cb42326638808f4
Ekaterina Volkova <koweevaea@gmail.com>
2024-12-11 22:25:11 +0500

    Добавила файл маппинга

---
+++ BlogAspNetMVC/MappingProfile.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using BlogAspNetMVC.BusinessLogic.Requests.ArticleRequests;
+using BlogAspNetMVC.BusinessLogic.Requests.CommentRequest;
+using BlogAspNetMVC.BusinessLogic.Requests.TagRequest;
+using BlogAspNetMVC.BusinessLogic.Requests.UserRequests;
+using BlogAspNetMVC.Data.Models;
+using BlogAspNetMVC.Data.Queries;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlogAspNetMVC
+{
+    public class MappingProfile : Profile
+    {
+        /// <summary>
+        /// конструктор: настройка соответствия сущностей 
+        /// </summary>
+        public MappingProfile()
+        {
+            CreateMap<SignUpRequest, User>();
+            CreateMap<AddNewArticleRequest, Article>();
+            CreateMap<AddNewCommentRequest, Comment>();
+            CreateMap<AddNewTagRequest, Tag>();
+            CreateMap<ChangeUserNameRequest, UpdateUserQuery>();
+            CreateMap<ChangePasswordRequest, UpdateUserQuery>();
+            CreateMap<ChangeArticleRequest, UpdateArticleQuery>();
+            CreateMap<ChangeCommentRequest, UpdateCommentQuery>();
+
+        }
+    }
+}
