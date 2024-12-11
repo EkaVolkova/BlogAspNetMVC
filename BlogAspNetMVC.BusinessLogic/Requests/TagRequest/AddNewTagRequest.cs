f3942f6d19d8ea0e809e8f82d8aae78d1655be15
Ekaterina Volkova <koweevaea@gmail.com>
2024-12-11 21:43:34 +0500

    Добавила запросы для тегов в бизнес-логику

---
+++ BlogAspNetMVC.BusinessLogic/Requests/TagRequest/AddNewTagRequest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogAspNetMVC.BusinessLogic.Requests.TagRequest
+{
+    public class AddNewTagRequest
+    {
+        public string Name { get; set; }
+    }
+}
---
+++ BlogAspNetMVC.BusinessLogic/Requests/TagRequest/ChangeTagRequest.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace BlogAspNetMVC.BusinessLogic.Requests.TagRequest
+{
+    public class ChangeTagRequest
+    {
+        public Guid Id { get; set; }
+        public string NewText { get; set; }
+    }
+}
