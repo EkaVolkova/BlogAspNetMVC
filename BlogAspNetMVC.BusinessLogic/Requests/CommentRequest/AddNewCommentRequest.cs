5fd2d9ff5e604d67e8541fc4e40c6c394a1311c5
Ekaterina Volkova <koweevaea@gmail.com>
2024-12-11 21:43:24 +0500

    Добавила запросы для комментариев в бизнес-логику

---
+++ BlogAspNetMVC.BusinessLogic/Requests/CommentRequest/AddNewCommentRequest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogAspNetMVC.BusinessLogic.Requests.CommentRequest
+{
+    public class AddNewCommentRequest
+    {
+        public Guid Guid { get; set; }
+        public string Text { get; set; }
+        public Guid ParentCommentId { get; set; }
+        public Guid AuthorId { get; set; }
+        public Guid ArtcleId { get; set; }
+    }
+}
---
+++ BlogAspNetMVC.BusinessLogic/Requests/CommentRequest/ChangeCommentRequest.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace BlogAspNetMVC.BusinessLogic.Requests.CommentRequest
+{
+    public class ChangeCommentRequest
+    {
+        public Guid Id { get; set; }
+        public string NewText { get; set; }
+    }
+}
