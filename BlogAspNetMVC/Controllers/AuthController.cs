7738993b79ce6620d2f2007c3f7b090f2397d464
VolkovaEkaterina <asu-dev007@yandex.ru>
2024-12-12 16:16:46 +0500

    Перенесла из контроллера пользователя в контроллер аутентификации регистрацию и аутентификацию пользователей

---
+++ BlogAspNetMVC/Controllers/AuthController.cs
@@ -0,0 +1,121 @@
+﻿using AutoMapper;
+using BlogAspNetMVC.BusinessLogic.Requests.RoleRequest;
+using BlogAspNetMVC.BusinessLogic.Requests.UserRequests;
+using BlogAspNetMVC.BusinessLogic.Services;
+using BlogAspNetMVC.BusinessLogic.ViewModels;
+using BlogAspNetMVC.Data.Models;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace BlogAspNetMVC.Controllers
+{
+    /// <summary>
+    /// Контроллер для работы с аутентификацией
+    /// </summary>
+    [Route("[controller]")]
+    public class AuthController : Controller
+    {
+        IUserService _userService;
+        IRoleService _roleService;
+        ILogger<AuthController> _logger;
+        IMapper _mapper;
+
+        public AuthController(ILogger<AuthController> logger, IUserService authService, IRoleService roleService, IMapper mapper)
+        {
+            _userService = authService;
+            _roleService = roleService;
+            _logger = logger;
+            _mapper = mapper;
+        }
+
+
+        /// <summary>
+        /// Функция аутентификации
+        /// </summary>
+        /// <param name="user"></param>
+        /// <returns></returns>
+        private async Task AuthentAsync(UserViewModel user)
+        {
+            
+            //Инициализируем список утверждений проверки подлинности
+            var claims = new List<Claim>()
+            {
+                //Добавляем одно утверждение с типом по умолчанию и логином пользователя
+                new Claim(ClaimsIdentity.DefaultNameClaimType, user.UserName),
+                //Добавляем одно утверждение с типом роли по умолчанию и ролью
+                new Claim(ClaimsIdentity.DefaultRoleClaimType, user.RoleName)
+            };
+
+            //Создали объект класса ClaimsIdentity, который реализует интерфейс IIdentity и представляет текущего пользователя
+            ClaimsIdentity claimsIdentity = new ClaimsIdentity(
+                claims,
+                "AppCookie",
+                ClaimsIdentity.DefaultNameClaimType,
+                ClaimsIdentity.DefaultRoleClaimType);
+
+            //Добавляем в контекст ClaimsPrincipal для работы с авторизацией, который содержит ClaimsIdentity
+            await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(claimsIdentity));
+
+        }
+
+        /// <summary>
+        /// Вход пользователя
+        /// </summary>
+        /// <param name="request">Запрос на вход</param>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("SignIn")]
+        public async Task<IActionResult> SignIn(
+            [FromBody] // Атрибут, указывающий, откуда брать значение объекта
+            SignInRequest request // Объект запроса
+            )
+        {
+            var result = await _userService.SignIn(request);
+            var status = (ObjectResult)result;
+
+            //Если вход прошел успешно, выполняем аутентификацию
+            if (status.StatusCode == 200)
+            {
+                
+                var user = (UserViewModel)status.Value;
+                await AuthentAsync(user);
+                return StatusCode((int)(status.StatusCode), user);
+            }
+
+            return StatusCode((int)(status.StatusCode), status.Value);
+
+        }
+
+        /// <summary>
+        /// Регистрация пользователя
+        /// </summary>
+        /// <param name="request">Запрос на регистрацию</param>
+        /// <returns></returns>
+        [HttpPost]
+        [Route("SignUp")]
+        public async Task<IActionResult> SignUp(
+            [FromBody] // Атрибут, указывающий, откуда брать значение объекта
+            SignUpRequest request // Объект запроса
+            )
+        {
+            var result = await _userService.SignUp(request);
+            var status = (ObjectResult)result;
+
+            //Если регистрация прошла успешно, выполняем аутентификацию
+            if (status.StatusCode == 200)
+            {
+                var user = (UserViewModel)status.Value;
+                await AuthentAsync(user);
+                return StatusCode((int)(status.StatusCode), user);
+            }
+            return StatusCode((int)(status.StatusCode), status.Value);
+
+        }
+
+    }
+}
--- BlogAspNetMVC/Controllers/UserController.cs
+++ BlogAspNetMVC/Controllers/UserController.cs
@@ -1,4 +1,5 @@
-﻿using BlogAspNetMVC.BusinessLogic.Requests.UserRequests;
+﻿using BlogAspNetMVC.BusinessLogic.Requests.RoleRequest;
+using BlogAspNetMVC.BusinessLogic.Requests.UserRequests;
 using BlogAspNetMVC.BusinessLogic.Services;
 using BlogAspNetMVC.Data.Repositories;
 using Microsoft.AspNetCore.Mvc;
@@ -22,32 +23,11 @@ namespace BlogAspNetMVC.Controllers
             _userService = userService;
         }
 
-        [HttpGet]
-        [Route("SignIn")]
-        public async Task<IActionResult> SignIn(
-            [FromBody] // Атрибут, указывающий, откуда брать значение объекта
-            SignInRequest request // Объект запроса
-            )
-        {
-            var result = await _userService.SignIn(request);
-            var status = (ObjectResult)result;
-            return StatusCode((int)(status.StatusCode), status.Value);
-
-        }
-
-        [HttpPost]
-        [Route("SignUp")]
-        public async Task<IActionResult> SignUp(
-            [FromBody] // Атрибут, указывающий, откуда брать значение объекта
-            SignUpRequest request // Объект запроса
-            )
-        {
-            var result = await _userService.SignUp(request);
-            var status = (ObjectResult)result;
-            return StatusCode((int)(status.StatusCode), status.Value);
-
-        }
-
+        /// <summary>
+        /// Изменить пароль
+        /// </summary>
+        /// <param name="request">Модель запроса на изменение пароля</param>
+        /// <returns></returns>
         [HttpPut]
         [Route("ChangePassword")]
         public async Task<IActionResult> ChangePassword(
@@ -62,6 +42,11 @@ namespace BlogAspNetMVC.Controllers
 
         }
 
+        /// <summary>
+        /// Изменить UserName
+        /// </summary>
+        /// <param name="request">Модель запроса на изменение UserName</param>
+        /// <returns></returns>
         [HttpPut]
         [Route("ChangeUserName")]
         public async Task<IActionResult> ChangeUserName(
@@ -76,6 +61,10 @@ namespace BlogAspNetMVC.Controllers
 
         }
 
+        /// <summary>
+        /// Получить список всех пользователей
+        /// </summary>
+        /// <returns></returns>
         [HttpGet]
         [Route("GetAllUsers")]
         public async Task<IActionResult> GetAllUsers()
@@ -85,6 +74,11 @@ namespace BlogAspNetMVC.Controllers
             return StatusCode((int)(status.StatusCode), status.Value);
         }
 
+        /// <summary>
+        /// Получить пользователя
+        /// </summary>
+        /// <param name="id">Идентификатор пользователя</param>
+        /// <returns></returns>
         [HttpGet]
         [Route("GetUserById{id}")]
         public async Task<IActionResult> GetUserById(
