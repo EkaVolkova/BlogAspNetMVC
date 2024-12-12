445b14c056d141cb988437e8318f26ce7249ca7b
VolkovaEkaterina <asu-dev007@yandex.ru>
2024-12-12 15:25:23 +0500

    Добавила модель запроса на обновление роли в БД

---
+++ BlogAspNetMVC.Data/Queries/UpdateRoleQuery.cs
@@ -0,0 +1,16 @@
+﻿namespace BlogAspNetMVC.Data.Queries
+{
+    public class UpdateRoleQuery
+    {
+
+        /// <summary>
+        /// Новое название роли
+        /// </summary>
+        public string NewName { get; set; }
+
+        public UpdateRoleQuery(string newName = null)
+        {
+            NewName = newName;
+        }
+    }
+}
