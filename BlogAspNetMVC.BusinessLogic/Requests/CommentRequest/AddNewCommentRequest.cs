dc3ba6f3692e56b8aaff5e5aa71bbfb2220c6474
Ekaterina Volkova <koweevaea@gmail.com>
2024-12-18 20:06:50 +0500

    Поправила орфографическую офишибку в запросе AddNewCommentRequest для бизнес-логики

--- BlogAspNetMVC.BusinessLogic/Requests/CommentRequest/AddNewCommentRequest.cs
+++ BlogAspNetMVC.BusinessLogic/Requests/CommentRequest/AddNewCommentRequest.cs
@@ -12,6 +12,6 @@ namespace BlogAspNetMVC.BusinessLogic.Requests.CommentRequest
         public string Text { get; set; }
         public Guid ParentCommentId { get; set; }
         public Guid AuthorId { get; set; }
-        public Guid ArtcleId { get; set; }
+        public Guid ArticleId { get; set; }
     }
 }
