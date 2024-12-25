63941c1f160f23efb9f692b74e630dbeee74309b
VolkovaEkaterina <asu-dev007@yandex.ru>
2024-12-25 15:45:20 +0500

    Добавила в Startup обработчик ошибок

--- BlogAspNetMVC/Startup.cs
+++ BlogAspNetMVC/Startup.cs
@@ -79,16 +79,9 @@ namespace BlogAspNetMVC
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
+            app.UseExceptionHandler("/Home/Error");
+            app.UseHsts();
+
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
@@ -96,6 +89,8 @@ namespace BlogAspNetMVC
 
             app.UseAuthentication();
             app.UseAuthorization();
+
+            app.UseStatusCodePagesWithReExecute("/Home/Error", "?statusCode={0}");
 
             app.UseEndpoints(endpoints =>
             {
